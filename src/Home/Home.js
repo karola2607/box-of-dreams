99221defce2f5b40c0b3960e04061cd3562eba8d
Karolina Pastor <borek.karola@gmail.com>
2021-12-12 18:50:49 +0100

    Add new components and delete unnecessary files

---
+++ src/Home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "../Container/Container";
+import styles from "./Home.css";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Container>
+        <div className="Home">
+          <header className="Home-header">
+            <img id="present" src="https://i.postimg.cc/pr9b3sjV/present.gif" />
+            <p id="headerTitel">Buy me a present!</p>
+          </header>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default Home;
