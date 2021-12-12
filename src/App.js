d7645cc2dd35b77fcbcd8f116dac21e75a17d735
Karolina Pastor <borek.karola@gmail.com>
2021-12-12 22:45:42 +0100

    Add react router, new components Info and MainLayout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Home from "./Home/Home.js";
+import Info from "./Info/Info.js";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import MainLayout from "./MainLayout/MainLayout.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        <BrowserRouter>
+          <MainLayout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/info" component={Info} />
+            </Switch>
+          </MainLayout>
+        </BrowserRouter>
       </header>
     </div>
   );
